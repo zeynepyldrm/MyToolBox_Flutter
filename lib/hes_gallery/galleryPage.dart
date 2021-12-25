38b0196d637fb6ee45ca2ce964579a4a7d5f8ff6
mrdlypenyez <zeynepyldrm1516@gmail.com>
2021-12-25 10:39:04 +0300

    gallery page theme bugfix

--- lib/hes_gallery/galleryPage.dart
+++ lib/hes_gallery/galleryPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'imageDetailsPage.dart';
 export 'package:yapilacaklar_listesi/hes_gallery/galleryPage.dart';
+
+import 'package:yapilacaklar_listesi/theming/ThemeModel.dart';
 
 List<Images> _images = [
   Images(imagePath: 'assets/hes.png'),
@@ -12,62 +15,62 @@ List<Images> _images = [
 class GalleryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Gallery"),
-        backgroundColor: Colors.grey,
-      ),
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                  vertical: 20,
-                ),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).accentColor,
-                ),
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
+    return Consumer(builder: (context, ThemeModel themeNotifier, child) {
+      Color bcColor =
+          themeNotifier.isDark ? Colors.white : Colors.grey.shade700;
+      return Scaffold(
+        backgroundColor: Colors.green,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
                   ),
-                  itemBuilder: (context, index) {
-                    return RawMaterialButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ImageDetailsPage(
-                              imagePath: _images[index].imagePath,
+                  decoration: BoxDecoration(
+                    color: bcColor,
+                  ),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      return RawMaterialButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ImageDetailsPage(
+                                imagePath: _images[index].imagePath,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: AssetImage(_images[index].imagePath),
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        );
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            image: AssetImage(_images[index].imagePath),
-                            fit: BoxFit.cover,
-                          ),
                         ),
-                      ),
-                    );
-                  },
-                  itemCount: _images.length,
+                      );
+                    },
+                    itemCount: _images.length,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
 
